f227acdbb389d77712239acb543861d60145b4c4
Sebalee <seba.nico.lee@gmail.com>
2025-12-30 23:51:00 -0300

    chore: precommit hook test v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
         width={100}
         height={20}
       />
-  <br />
-  <p>TODO</p>
-  <p>precommit hook test</p>
+      <br />
+      <p>TODO</p>
+      <p>precommit hook test</p>
     </div>
   );
 }
