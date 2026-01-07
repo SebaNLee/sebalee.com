7dcfd9e929a260b012af8868af19e35eeab80268
SebaNLee <seba.nico.lee@gmail.com>
2026-01-07 13:25:20 -0300

    build: fix? assets not loading

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.PAGES_BASE_PATH,
   assetPrefix: process.env.PAGES_BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
