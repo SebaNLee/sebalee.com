003bb08fd437b6b0970257b54bdeebc885d09226
SebaNLee <seba.nico.lee@gmail.com>
2026-01-08 18:32:34 -0300

    chore: boids bg is layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BoidsBackground from "@/components/animations/boids-background";
+import BoidsBackground from "@/components/layout/boids-background";
 import Script from "next/script";
 
 const geistSans = Geist({
