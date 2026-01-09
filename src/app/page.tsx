8accf23e48b1936cb070350afd95a2fa2b7f9225
SebaNLee <seba.nico.lee@gmail.com>
2026-01-09 15:18:39 -0300

    feat!: hud layer wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import { HudBar } from "@/components/hud/bar/hud-bar";
 import { AnmRevealText } from "@/components/animation/scanner-text/anm-scanner-text";
+import HudLayer from "@/components/hud/hud-layer";
 
 export default function Home() {
   return (
     <>
       <>
         {/* TODO debug */}
-        <HudBar x={140} y={42} delay={500} length={300} />
-        <HudBar x={140} y={42} delay={1000} length={300} vertical />
+        <HudLayer>
+          <HudBar x={600} y={20} delay={500} length={300} />
+          <HudBar x={600} y={20} delay={1000} length={300} vertical />
+        </HudLayer>
       </>
       <div className="">
         <Image
