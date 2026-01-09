b7a4c767c43b9a67473e9053f6c05bfd290cab24
SebaNLee <seba.nico.lee@gmail.com>
2026-01-09 00:23:30 -0300

    feat: font management now in layout.tsx and not globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistMono.className} ${geistSans.className} antialiased`}
       >
         {/* Google tag (gtag.js) */}
         <Script
