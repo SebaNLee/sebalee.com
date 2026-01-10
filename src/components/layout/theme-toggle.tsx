870d94e86f247d88158a3386b33bd206ad56188d
SebaNLee <seba.nico.lee@gmail.com>
2026-01-10 18:59:53 -0300

    Revert "fix: toggle hydration mismatch warning"
    This reverts commit c5a351c81f3162dae096e0681cf5141d3b88933d.

--- src/components/layout/theme-toggle.tsx
+++ src/components/layout/theme-toggle.tsx
@@ -1,20 +1,9 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 export function ThemeToggle() {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-
-  useEffect(() => {
-    // eslint-disable-next-line
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
 
   return (
     // TODO better UI
