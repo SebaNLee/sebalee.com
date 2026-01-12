bb239c32cc92e4e29dcc4cc2b47f14d5cb8f8a6f
SebaNLee <seba.nico.lee@gmail.com>
2026-01-12 15:54:04 -0300

    chore: font tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,11 +39,27 @@ export default function Home() {
           TODO:
           <p>second test</p>
         </AnmRevealText>
-        {/* <div className="items-end justify-items-center">
-            <p className="font-ava text-4xl">Sebastian Nicolas Lee</p>
-            <p className="font-ibm text-xs">Software Engineering Student</p>
-            <p className="font-ibm-logo text-x1">Test</p>
-          </div> */}
+        <div className="items-end justify-items-center">
+          {/* <p className="font-ava text-4xl">Sebastian Nicolas Lee</p>
+          <p className="font-ibm text-xs">Software Engineering Student</p>
+          <p className="font-ibm-logo text-x1">Test</p>
+          <br />
+          <p className="font-helvetica text-4xl">Sebastian Nicolas Lee</p>
+          <p className="font-helvetica text-xs">Software Engineering Student</p>
+          <p className="font-helvetica text-x1">Test</p>
+          <br />
+          <p className="font-inter text-4xl">Sebastian Nicolas Lee</p>
+          <p className="font-inter text-xs">Software Engineering Student</p>
+          <p className="font-inter text-x1">Test</p>
+          <br />
+          <p className="font-satoshi text-4xl">Sebastian Nicolas Lee</p>
+          <p className="font-satoshi text-xs">Software Engineering Student</p>
+          <p className="font-satoshi text-x1">Test</p>
+          <br />
+          <p className="text-4xl">Sebastian Nicolas Lee</p>
+          <p className="text-xs">Software Engineering Student</p>
+          <p className="text-x1">Test</p> */}
+        </div>
         <div>
           <br />
           <AnmRevealText delay={900}>
