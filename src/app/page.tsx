17f4ef5b7fb6c25c11b3a215991feed6f5688349
SebaNLee <seba.nico.lee@gmail.com>
2026-01-12 22:29:08 -0300

    feat!: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
 import Image from "next/image";
-import { AnmBar } from "@/components/animation/bar/anm-bar";
-import { AnmScannerText } from "@/components/animation/scanner-text/anm-scanner-text";
-import { AnmScanner } from "@/components/animation/scanner/anm-scanner";
-import Link from "next/link";
 import { AnmRevealText } from "@/components/animation/reveal-text/anm-reveal-text";
 import { AnmRevealGrid } from "@/components/animation/reveal-grid/anm-reveal-grid";
 
@@ -13,34 +9,65 @@ export default function Home() {
       <div className="absolute inset-0 pointer-events-none"></div>
       <div>
         <AnmRevealText delay={500}>
-          <div>
-            <p className="font-satoshi text-4xl">Sebastian Nicolas Lee</p>
-            <p className="font-satoshi text-xs">Software Engineering Student</p>
-            <p className="font-satoshi text-x1">Test</p>
+          <div
+            className="
+            w-full h-full gap-1
+            flex flex-col md:flex-row
+            items-center
+            justify-start md:justify-center
+            pt-[40%] md:pt-0
+            px-6 md:px-10 2xl:p-40
+            "
+          >
+            <div className="flex flex-col md:flex-1 md:flex-col items-center md:items-start">
+              <p className="font-satoshi text-4xl font-thin md:text-6xl 2xl:text-7xl">
+                SEBASTIÁN NICOLÁS LEE
+              </p>
+              <p className="font-satoshi text-xs md:text-lg 2xl:text-xl">
+                Software Engineering Student
+              </p>
+            </div>
+
+            <div
+              className="
+              text-justify
+              flex flex-col md:flex-col
+              md:w-[35vw]
+              items-center md:items-start
+              mt-auto md:mt-0
+              mb-3 md:mb-0
+              gap-2 md:gap-4
+              text-xs md:text-sm 2xl:text-base
+              px-12 md:px-0
+              "
+            >
+              <p>Welcome to my website!</p>
+              <p>
+                Pleased to make your acquaintance. I&apos;s Sebalee. I always
+                like having an active project; if it&apos;s not on GitHub
+                it&apos;s usually some geeky offline stuff.{" "}
+              </p>
+              <p>
+                Enjoy low-level work, system architecture design, and classical
+                ML. I&apos;ve learned web development too. Fun, but not my
+                endgame.
+              </p>
+              <p>
+                Outside tech, I generally enjoy learning new stuff: love
+                reading, drinking good tea and mate, video games (mostly indie
+                games) and tinkering with electronics.
+              </p>
+
+              <p className="flex flex-row underline text-xs md:text-md gap-5 mt-8 md:mt-8">
+                <a href="https://github.com/SebaNLee">GitHub</a>
+                <a href="https://www.linkedin.com/in/sebalee/">LinkedIn</a>
+                <a href="https://mailto:seba.nico.lee@gmail.com">
+                  seba.nico.lee@gmail.com
+                </a>
+              </p>
+            </div>
           </div>
         </AnmRevealText>
-
-        {/* <div className="items-end justify-items-center">
-          <p className="font-ava text-4xl">Sebastian Nicolas Lee</p>
-          <p className="font-ibm text-xs">Software Engineering Student</p>
-          <p className="font-ibm-logo text-x1">Test</p>
-          <br />
-          <p className="font-helvetica text-4xl">Sebastian Nicolas Lee</p>
-          <p className="font-helvetica text-xs">Software Engineering Student</p>
-          <p className="font-helvetica text-x1">Test</p>
-          <br />
-          <p className="font-inter text-4xl">Sebastian Nicolas Lee</p>
-          <p className="font-inter text-xs">Software Engineering Student</p>
-          <p className="font-inter text-x1">Test</p>
-          <br />
-          <p className="font-satoshi text-4xl">Sebastian Nicolas Lee</p>
-          <p className="font-satoshi text-xs">Software Engineering Student</p>
-          <p className="font-satoshi text-x1">Test</p>
-          <br />
-          <p className="text-4xl">Sebastian Nicolas Lee</p>
-          <p className="text-xs">Software Engineering Student</p>
-          <p className="text-x1">Test</p>
-        </div> */}
       </div>
     </>
   );
