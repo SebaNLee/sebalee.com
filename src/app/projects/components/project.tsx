975045f37718183bcc9cc503184e3e25d2fc0967
SebaNLee <seba.nico.lee@gmail.com>
2026-01-13 12:49:09 -0300

    feat: add project icons support

--- src/app/projects/components/project.tsx
+++ src/app/projects/components/project.tsx
@@ -3,10 +3,11 @@ import { FiArrowUpRight } from "react-icons/fi";
 type Props = {
   name: string;
   link: string;
+  icons?: string[];
   description: string;
 };
 
-export function Project({ name, link, description }: Props) {
+export function Project({ name, link, icons, description }: Props) {
   return (
     <div
       className="
@@ -14,16 +15,28 @@ export function Project({ name, link, description }: Props) {
       mb-4 md:mb-6 2xl:mb-8
       "
     >
-      <a
-        href={link}
+      <p
         className="
-        underline flex items-center
+        flex items-center
         text-sm md:text-base 2xl:text-xl
         "
       >
-        {name}
-        <FiArrowUpRight />
-      </a>
+        <a href={link} className="underline flex items-center">
+          {name}
+          <FiArrowUpRight />
+        </a>
+        <p className="ml-2 flex flex-row justify-center items-center gap-1 text-xs 2xl:text-base">
+          {icons?.map((item) =>
+            item.startsWith("devicon-") ? (
+              <i key={item} className={item}></i>
+            ) : (
+              <span key={item} className="font-mono text-xs">
+                {item}
+              </span>
+            ),
+          )}
+        </p>
+      </p>
       <p
         className="
         text-xs md:text-sm 2xl:text-base
