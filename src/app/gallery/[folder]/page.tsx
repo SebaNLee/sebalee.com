a57f677a1c5a9cd8ca6c2020d718cfd4cb1adc19
SebaNLee <seba.nico.lee@gmail.com>
2026-01-13 16:03:49 -0300

    feat: gallery folder page

---
+++ src/app/gallery/[folder]/page.tsx
@@ -0,0 +1,48 @@
+import { AnmRevealGrid } from "@/components/animation/reveal-grid/anm-reveal-grid";
+import { GALLERIES } from "../page";
+import { AnmRevealText } from "@/components/animation/reveal-text/anm-reveal-text";
+
+type Props = {
+  params: Promise<{
+    folder: string;
+  }>;
+};
+
+export async function generateStaticParams() {
+  return GALLERIES.map((gallery) => ({ folder: gallery.slug }));
+}
+
+export default async function GalleryFolder({ params }: Props) {
+  const { folder } = await params;
+
+  return (
+    <>
+      <AnmRevealGrid invertOpacity />
+      <div>
+        <AnmRevealText delay={500}>
+          <div
+            className="
+            w-full h-full gap-1
+            flex flex-col md:flex-row
+            items-center
+            justify-start md:justify-center
+            pt-[8%] md:pt-0
+            px-1 md:px-10 2xl:p-40
+            "
+          >
+            <div className="flex flex-col md:flex-1 md:flex-col items-center md:items-start">
+              <p className="font-satoshi text-3xl font-normal md:font-thin md:text-6xl 2xl:text-7xl">
+                GALLERY
+              </p>
+              <p className="font-satoshi text-[0.6em] md:text-lg 2xl:text-xl">
+                {folder}
+              </p>
+            </div>
+
+            <div>{/* TODO masontry gallery */}</div>
+          </div>
+        </AnmRevealText>
+      </div>
+    </>
+  );
+}
