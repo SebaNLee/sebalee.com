1a0812aa4b5a0e36f8b5bd6dfd2168b01d3d3731
SebaNLee <seba.nico.lee@gmail.com>
2026-01-16 01:34:30 -0300

    chore: sitemap for seo

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://sebalee.com";
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
